62a861930b33b9beffdedc980432739e18d61d4b
arka bhowmick <arka_bhowmick@yahoo.in>
2018-01-31 14:50:07 +0530

    parallax background added.... some new features added

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
   $(window).scroll(function(){
-    console.log($(this).scrollTop());
+
 
     if($(this).scrollTop()>210){
-      console.log('hi');
-      $('header').addClass('fixed-nav');
+
+      $('header').addClass("fixed-nav");
     }
     else{
       $('header').removeClass('fixed-nav');
