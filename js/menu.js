eeb77401cfccc3341f45bb828b397bd6b09853c5
arka bhowmick <arka_bhowmick@yahoo.in>
2018-02-01 15:39:30 +0530

    new navigation bar added with effects

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,13 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
+    var headerHeight=50;
 
-  $("#menu").on("click",function(){
-
-    $("#main-nav ul").toggleClass("open");
-
-  });
-});*/
+    $(".menu-item , #logo").click(function(e){
+        var link=$(this).attr("href");
+        $('body , html').animate({
+            scrollTop : $(link).offset().top - headerHeight
+        },1000);
+        $(".menu-item").children().removeClass("active");
+        $(this).children().addClass("active");
+        e.preventDefault();
+    });
+});
