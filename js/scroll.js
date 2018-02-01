aa06b294b8936d3a22aa0e40ce44bb92f28b3349
arka bhowmick <arka_bhowmick@yahoo.in>
2018-02-01 11:30:36 +0530

    new navigation menu added

--- js/scroll.js
+++ js/scroll.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
   $(window).scroll(function(){
     console.log($(this).scrollTop());
     if($(document).scrollTop()>210){
-        $("header").css({'height':'50px','backgroundColor':'black'});
+        $("#header").css({'height':'50px','border':'0px','paddingTop':'0','backgroundColor':'black'});
     }
     else if($(document).scrollTop()<210){
-        $("header").css({'height':'120px','backgroundColor':'rgba(0,0,0,0)'});
+        $("#header").css({'height':'120px','borderTop':'8px solid #ffd800','paddingTop':'50px','backgroundColor':'rgba(0,0,0,0)'});
     }
   });
 });
