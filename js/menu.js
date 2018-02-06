fea9acc0a03f71c3a1125c7e9547fbc3a4474d4f
arka bhowmick <arka_bhowmick@yahoo.in>
2018-02-06 18:11:31 +0530

    menu animation done...more things added

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     var headerHeight=50;
-
     $(".menu-item , #logo").click(function(e){
         var link=$(this).attr("href");
         $('body , html').animate({
@@ -10,4 +9,18 @@ $(document).ready(function(){
         $(this).children().addClass("active");
         e.preventDefault();
     });
+
+
+    $("#menu-icon").click(function(){
+        $(this).children("div").toggleClass("cross");
+        $("#menu").toggleClass("expand");
+    });
+    $( window ).resize(function() {
+        if($(this).width()>=768){
+            $("#menu").removeClass("expand");
+            $("#menu-icon").children("div").removeClass("cross");
+        }
+    });
+
+
 });
