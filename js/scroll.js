2109926f2cf22058c28fcf76a0c56a655bc010aa
arka bhowmick <arka_bhowmick@yahoo.in>
2018-02-06 11:12:10 +0530

    awesome app section done

--- js/scroll.js
+++ js/scroll.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $(".preload").fadeOut("slow");
   $(window).scroll(function(){
     if($(document).scrollTop()>210){
-        $("#header").css({'height':'50px','border':'0px','paddingTop':'0','backgroundColor':'black'});
+        $("#header").css({'height':'60px','paddingTop':'0','backgroundColor':'black'});
     }
     else if($(document).scrollTop()<210){
         $("#header").css({'height':'120px','borderTop':'8px solid #ffd800','paddingTop':'50px','backgroundColor':'rgba(0,0,0,0)'});
