3f2bfdf052dde22fd18d8cb295e535850131950f
arka bhowmick <arka_bhowmick@yahoo.in>
2018-02-20 15:55:29 +0530

    made all portion with animate.css wow.js parallax.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,52 +1,71 @@
 
 var headerHeight=60;
 $(document).ready(function(){
-  $(".preload").fadeOut("slow");
-  $(window).scroll(function(){
+    checkHeader();
+    hideElements();
+    setTimeout(function() {
+        $(".preload").fadeOut("slow");
+    }, 200);
+
+    headerAdjust();
+    menuActive();
+    animate();
+});
+
+
+function deactive(){
+    $(".menu-item").children().removeClass("active");
+}
+
+function checkHeader(){
     if($(document).scrollTop()>210){
         $("#header").css({'height':'80px','paddingTop':'0','backgroundColor':'black'});
     }
     else if($(document).scrollTop()<210){
         $("#header").css({'height':'120px','borderTop':'8px solid #ffd800','paddingTop':'50px','backgroundColor':'rgba(0,0,0,0)'});
     }
+}
+function headerAdjust(){
+    $(window).scroll(function(){
+        checkHeader();
+    });
+}
 
-    //console.log($("#team"));
-    /*console.log("document top: " + $(document).scrollTop());
-    console.log("team document top: " + $("#team").offset().top);
-    console.log("team 1 top: " + $("#team1").offset().top);*/
-    if($(document).scrollTop()>=$("#team").offset().top-70){
-      $(".team-desc").children("img").css({"right":"0px","transform":"rotate(360deg)","opacity": "1","filter": "alpha(opacity=100)"});
-      $(".team-desc").children(".text").css({"opacity": "1","filter": "alpha(opacity=100)"});
-    }
+function menuActive(){
+    $(window).scroll(function(){
+        if($(document).scrollTop()>$("#work").offset().top - headerHeight){
+            deactive();
+            $("#work-menu").addClass("active");
+        }
+        else if($(document).scrollTop()>$("#download").offset().top - headerHeight){
+            deactive();
+            $("#download-menu").addClass("active");
+        }
+        else if($(document).scrollTop()>$("#team").offset().top - headerHeight){
+            deactive();
+            $("#team-menu").addClass("active");
+        }
+        else if($(document).scrollTop()>$("#services").offset().top - headerHeight){
+            deactive();
+            $("#services-menu").addClass("active");
+        }
+        else if($(document).scrollTop()>$("#main").offset().top - headerHeight){
+            deactive();
+            $("#main-menu").addClass("active");
+        }
+    });
+}
 
+function hideElements(){
+    $(".animation").css({"visibility":"hidden"});
+    //$("#header").hide();
+}
 
-    if($(document).scrollTop()>$("#work").offset().top - headerHeight){
-        deactive();
-        $("#work-menu").addClass("active");
-        $("#work").children(".container").css({"opacity":"1","filter": "alpha(opacity=100)"});
-    }
-    else if($(document).scrollTop()>$("#download").offset().top - headerHeight){
-        deactive();
-        $("#download-menu").addClass("active");
-    }
-    else if($(document).scrollTop()>$("#team").offset().top - headerHeight){
-        deactive();
-        $("#team-menu").addClass("active");
-    }
-    else if($(document).scrollTop()>$("#services").offset().top - headerHeight){
-        deactive();
-        $("#services-menu").addClass("active");
-    }
-    else if($(document).scrollTop()>$("#main").offset().top - headerHeight){
-        deactive();
-        $("#main-menu").addClass("active");
-    }
+function animate(){
+    $("#header").show("slow","swing",function(){
+        //$("#head-1").addClass("wow fadeIn");
+    });
+    $(window).scroll(function(){
 
-
-  });
-});
-
-
-function deactive(){
-    $(".menu-item").children().removeClass("active");
+    });
 }
